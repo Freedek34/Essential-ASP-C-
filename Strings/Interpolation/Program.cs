b2820946d92e1a9de8558fb3ea7283caeedd2bac
Freedek34 <144965861+Freedek34@users.noreply.github.com>
2025-02-03 09:48:14 +0100

    Fixed string formatting issues in C# program

--- Strings/Interpolation/Program.cs
+++ Strings/Interpolation/Program.cs
@@ -1,20 +1,28 @@
-﻿// Application Programming .NET Programming with C# by Abdullahi Tijjani
+﻿using System;
 
-// String Interpolation is a feature that enables the easy insertion of data
-// and expression values into a string variable
+class Program
+{
+    static void Main()
+    {
+        // String Interpolation is a feature that enables the easy insertion of data
+        // and expression values into a string variable
 
-int a = 100;
-float b = 250.0f;
-string c = "CSharp";
+        int a = 100;
+        float b = 250.0f;
+        string c = "CSharp";
 
-// String output the old way - using placeholders
-Console.WriteLine("The values are {0}, {1} and {2}", a, b, c);
+        // String output the old way - using placeholders
+        Console.WriteLine("The values are {0}, {1} and {2}", a, b, c);
 
-// TODO: Using string interpolation, the code is much easier to read
+        // TODO: Using string interpolation, the code is much easier to read
+        Console.WriteLine($"The values are {a}, {b} and {c}");
 
+        // TODO: Interpolated strings can contain expressions as well
+        Console.WriteLine($"(a + b)/b is {(a + b) / b}");
+        Console.WriteLine($"{c} in upper-case is {c.ToUpper()}");
 
-// TODO: Interpolated strings can contain expressions as well
-
-
-// TODO: Complex objects can be embedded in strings this way as well
-
+        // TODO: Complex objects can be embedded in strings this way as well
+        DateTime now = DateTime.Now;
+        Console.WriteLine($"Today is {now}");
+    }
+}
